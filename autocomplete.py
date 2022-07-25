460b69c0b1e1614cf9ba30e1fd1b1b4adb60ea9d
Michal Bucks <michall1900@gmail.com>
2022-07-25 16:30:38 +0300

    An idea but it is not good..

--- autocomplete.py
+++ autocomplete.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from os.path import exists
 import re
 from typing import List
 
@@ -15,6 +16,26 @@ class AutoCompleteData:
         self.offset = offset
         self.score = score
 
+
+# def find_all_matches(words_list: List[str], ) -> List[AutoCompleteData]:
+#     matches = []
+#     start_position = 0
+#     for word in words_list:
+#         path = "../" + word
+#         if len(word) <= 2:
+#             continue
+#         if len(word) >= 3 and not exists(path):
+#             return []
+#
+#
+#
+#
+# def get_best_k_completions(prefix: str) -> List[AutoCompleteData]:
+#     words_list = re.findall(r"\W*\w+\W*", prefix)
+#     matches_found = find_all_matches(words_list)
+#     if len(matches_found):
+#         return matches_found
+#     return list_after_change_letters()
 
 if __name__ == '__main__':
     pass
