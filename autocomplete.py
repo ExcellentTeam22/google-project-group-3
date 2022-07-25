4916ed57a52ad14a4c2d1a088b803001ad496aa2
Michal Bucks <michall1900@gmail.com>
2022-07-25 16:37:13 +0300

    Add word details class

---
+++ autocomplete.py
@@ -0,0 +1,12 @@
+class WordDetails:
+    def __init__(self, word:str, row_num: int, word_count: int, word_position: int, start_row_position: int):
+        self.word = word
+        self.row_num = row_num
+        self.word_count = word_count
+        self.word_position = word_position
+        self.start_row_position = start_row_position
+
+
+
+if __name__ == '__main__':
+    pass
