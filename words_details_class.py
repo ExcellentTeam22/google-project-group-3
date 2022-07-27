3afe229304c8203e7cc37e8f9f2a8c29e8815c52
Michal Bucks <michall1900@gmail.com>
2022-07-27 13:08:56 +0300

    Returned object fixed.

--- words_details_class.py
+++ words_details_class.py
@@ -8,13 +8,46 @@ class WordDetails:
     :ivar file_path: The path to the file where the current word has been found.
     """
     def __init__(self, word: str, row_num: int, word_place: int, start_row_position: int,
-                 file_path: str):
+                 file_path: str, full_row: str):
         self.word = word
         self.row_num = row_num
         self.word_place = word_place
         self.start_row_position = start_row_position
         self.file_path = file_path
         self.next = None
+        self.full_row = full_row
 
     def set_next(self, next_word: "WordDetails"):
         self.next = next_word
+
+    @property
+    def word(self):
+        return self.__word
+
+    @word.setter
+    def word(self, new_word: str):
+        self.__word = new_word
+
+    @property
+    def file_path(self):
+        return self.__file_path
+
+    @file_path.setter
+    def file_path(self, file_path: str):
+        self.__file_path = file_path
+
+    @property
+    def start_row_position(self):
+        return self.__start_row_position
+
+    @start_row_position.setter
+    def start_row_position(self, start_row_position: int):
+        self.__start_row_position = start_row_position
+
+    @property
+    def full_row(self):
+        return self.__full_row
+
+    @full_row.setter
+    def full_row(self, full_row: int):
+        self.__full_row = full_row
